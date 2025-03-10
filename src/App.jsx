9c2cde6dd16e66fabb51dfcde8bf41f26ab93f86
ggpio18 <piolo18aranza@gmail.com>
2025-03-10 17:00:59 +0800

    +=[preloader]

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
+"use client";
+import { useState, useEffect } from "react";
 import { Footer } from "./components/Footer";
 import { Navbar } from "./components/Navbar";
 import { About } from "./pages/About";
 import { Hero } from "./pages/Hero";
 import { Portfolio } from "./pages/Portfolio";
 import { Stack } from "./pages/Stack";
-
+import Loader from "./components/Loader"; // Import the Loader component
+import "./index.css"
 export default function App() {
+  const [loading, setLoading] = useState(true); // State to track loading
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); // Hide the loader after 3 seconds
+    }, 3000);
+
+    return () => clearTimeout(timer); // Cleanup timeout
+  }, []);
+
   return (
     <div>
-    <Navbar/>
-     <Hero/>
-     <About/>
-     <Portfolio/>
-     <Stack/>
-     <Footer/>
+      {loading ? (
+        <div className="preloader-container">
+          <Loader />
+        </div>
+      ) : (
+        <main className="fade-in">
+          <Navbar />
+          <Hero />
+          <About />
+          <Portfolio />
+          <Stack />
+          <Footer />
+        </main>
+      )}
     </div>
-  )
+  );
 }
